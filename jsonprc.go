a361382c3fb0a0856cb264c97db8c2f2e3d25f1a
MaxGridasoff <maxim.grid@proton.me>
2024-02-14 22:08:04 +0200

    add readme

--- jsonprc.go
+++ jsonprc.go
@@ -304,22 +304,3 @@ func (srv *Server) Handler(data []byte) []byte {
 	return srv.buildResponse(response)
 
 }
-
-/*
-func main() {
-	println()
-	tmp := &Service{}
-
-	server := NewServer()
-
-	_ = server.Register("service", tmp)
-
-	// NEXT: вытащить в отдельный package
-	// желательно придумать, как написать тесты
-
-	server.Handler([]byte(`{"jsonrpc": "2.0", "method": "service.Method2", "params": [1], "id":"1"}`))
-	server.Handler([]byte(`{"jsonrpc": "2.0", "method": "servic1e.Method3", "params": [[1,2,7]], "id":"1"}`))
-	server.Handler([]byte(`{"jsonrpc": "2.0", "method": "service.Method4", "params": [[1,2]], "id":"1"}`))
-	server.Handler([]byte(`{"jsonrpc": "2.0", "method": "service.Method5", "params": [{"name":"maxim", "phones":[123,456]}], "id":"1"}`))
-}
-*/
